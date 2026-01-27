60c25018e4da5ddac11ec727ea49c8a09bb7637e
kenaaaanz <gichabakenani@gmail.com>
2026-01-27 14:45:14 +0300

    commit 6 database

--- mookh_system/settings.py
+++ mookh_system/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -62,18 +63,31 @@ WSGI_APPLICATION = 'mookh_system.wsgi.application'
     #}
 #}
 
-DATABASES = {
-    'default': {
+#DATABASES = {
+    #'default': {
          #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "mookh_tms",
-        "USER": "postgres",
-        "PASSWORD": "Ken@4427",
-        "HOST": "localhost",
-        "PORT": "5432",
+        #"ENGINE": "django.db.backends.postgresql",
+        #"NAME": "mookh_tms",
+        #"USER": "postgres",
+        #"PASSWORD": "Ken@4427",
+        #"HOST": "localhost",
+        #"PORT": "5432",
+    #}
+#}
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    # Production - PostgreSQL on Render
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True  # Important for Render
+        )
     }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
